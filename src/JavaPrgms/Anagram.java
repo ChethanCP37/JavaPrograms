fb2897c6a96c16151c1621fca24c08a328deb85f
Chethan CP <chethanrit37@gmail.com>
2021-11-16 23:01:24 +0530

    Update Anagram.java

--- src/JavaPrgms/Anagram.java
+++ src/JavaPrgms/Anagram.java
@@ -1,37 +1,41 @@
-package JavaPrgms;
+package GeneralJavaPrgms;
+
+import java.util.Arrays;
 
 public class Anagram {
 
-	/*
-	 * First remove the spaces of both the strings
-	 * then covert both strings to lower case
-	 * then compare each letter of one array with other array 
-	 * But below prgm won't work if we enter keepa and peekb, in that case use Arrays.Sort()
-	 * 
-	 * 
-	 */
-
 	public static void main(String[] args) {
+		boolean status = true;
 		String str1= "keep";
-		String str2 = "The Classroom";
+		String str2 = "peeK";
 
-		//Remove the spaces
+		//Remove the spaces and convert to lower case
 		str1 =str1.replaceAll("\\s","").toLowerCase();
 		str2 =str2.replaceAll("\\s","").toLowerCase();
-		if(str1.length()==str2.length()) {
-			for(int i=0;i<str1.length();i++) {
-				for(int j=i;j<str2.length();j++) {
-					if(str1.charAt(i)==str2.charAt(j)) {
-					break;
-					}
-				}
+
+		//If length of given two words are not same then it's not anagram
+		if(str1.length()!=str2.length()) {
+			status = false;
+		}
+
+		char ch[] = str1.toCharArray();
+		char ch1[] = str2.toCharArray();
+
+		Arrays.sort(ch);
+		Arrays.sort(ch1);
+
+		for(int i=0;i<str1.length();i++) {
+			if(ch[i]!=ch1[i]) {
+				status=false;
+				break;
 			}
+		}
+
+		if(status) {
 			System.out.println(str1+" and "+str2+" are Anagram");
 		}
 		else {
 			System.out.println(str1+" and "+str2+" are not Anagram");
 		}
-
 	}
-
 }
