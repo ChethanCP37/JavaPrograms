1fd2f235dfad294c887fd73d9b6b4876465058a2
Chethan CP <chethanrit37@gmail.com>
2021-11-16 23:11:46 +0530

    Update AssetEqual.java

--- src/JavaPrgms/AssetEqual.java
+++ src/JavaPrgms/AssetEqual.java
@@ -2,16 +2,17 @@ package JavaPrgms;
 
 public class AssetEqual {
 
+	// check st1 string is present in st string or not
 	public static void main(String[] args) {
 		String st = "I am god";
 		String st1 ="ama";
 		
 		boolean res = st.contains(st1)?true:false;
 		if(res==true) {
-			System.out.println(st1+" present");
+			System.out.println(st1+" present in the string "+st);
 		}
 		else {
-			System.out.println(st1+" Not present in "+ st);
+			System.out.println(st1+" is not present in the string "+ st);
 		}
 	}
 
